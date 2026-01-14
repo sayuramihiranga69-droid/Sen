bcb7fef96dc706955df3b2dc28bbb2546b45d19f
official <sayuramihiranga69@gmail.com>
2026-01-14 09:39:17 +0530

    Update Animegirl.js

--- plugins/Animegirl.js
+++ plugins/Animegirl.js
@@ -1,5 +1,5 @@
-const config = require('../config')
-const { cmd, commands } = require('../command')
+const config = require('../config');
+const { cmd } = require('../command');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
 const axios = require('axios');
 const cheerio = require('cheerio');
